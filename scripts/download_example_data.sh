b3732e2ea89bc43141f34437b86319154a86df33
Shintaro Sakoda <sakoda@keio.jp>
2023-05-12 21:19:19 +0000

    Added download_example_data.sh

---
+++ scripts/download_example_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -eux
+
+cd $(dirname $0)
+mkdir -p ../data
+cd ../data
+wget http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/nerf_example_data.zip
+unzip nerf_example_data.zip
